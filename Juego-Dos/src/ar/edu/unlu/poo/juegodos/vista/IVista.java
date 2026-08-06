e4f2cde115c80743531d514c45d180db4b08859c
Leandro Jaime <83981236+2442-pixeles@users.noreply.github.com>
2026-08-05 22:36:00 -0300

    Delete Juego-Dos directory

--- Juego-Dos/src/ar/edu/unlu/poo/juegodos/vista/IVista.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unlu.poo.juegodos.vista;
-
-import ar.edu.unlu.poo.juegodos.controlador.*;
-import ar.edu.unlu.poo.juegodos.modelo.*;
-
-public interface IVista {
-    void setControlador(Controlador controlador);
-}
-
